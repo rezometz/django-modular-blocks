f4076a152285fe713f9b2204092d07c689c2bad3
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2015-08-19 15:27:31 +0200

    translation files autocompilation

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
+import os
+import sys
 from setuptools import setup, find_packages
+from django.core import management
 
 import os
 
 modular_blocks = __import__('modular_blocks')
 
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+# translation files compilation
+currentdir = os.getcwd()
+os.chdir(os.path.join(currentdir, 'modular_blocks'))
+management.call_command('compilemessages', stdout=sys.stdout)
+os.chdir(currentdir)
 
 setup(
     name='django-modular-blocks',
